8b25b122a8ff10c666aa824b492e83295cfd7abb
Kashyap297 <kashyap29700@gmail.com>
2024-05-02 22:47:43 +0530

    setup login w/o JWT

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,7 +6,7 @@ const userController = {
         try {
             const { username, email, password } = req.body
             const user = await userModel.findOne({ email })
-            console.log(req.body)
+
             if (user) {
                 return res.status(400).json({
                     message: 'Email already Exist',
@@ -19,7 +19,7 @@ const userController = {
             const hashedPassword = await bcryptjs.hash(password, _SALT_ROUND)
             console.log(hashedPassword)
 
-            const data = await userModel.create({ username, email, password : hashedPassword })
+            const data = await userModel.create({ username, email, password: hashedPassword })
             res.status(201).json({
                 message: 'SignUp Successfully',
                 success: true,
@@ -29,6 +29,40 @@ const userController = {
             console.error('Error creating user:', error);
             res.status(500).json({
                 message: 'Error while creating user in DB',
+                success: false
+            })
+        }
+    },
+    login: async (req, res) => {
+        try {
+            const { email, password } = req.body
+            const user = await userModel.findOne({ email })
+
+            if (!user) {
+                return res.status(401).json({
+                    message: "Invalid Email Or Password",
+                    success: false
+                })
+            }
+
+            const isVerify = await bcryptjs.compare(password, user.password)
+
+            if (!isVerify) {
+                return res.status(401).json({
+                    message: "invalid email or password",
+                    success: false
+                })
+            }
+
+            res.json({
+                message: "Login Successfull",
+                success: true
+            })
+
+        } catch (error) {
+            console.error('Error while login user:', error);
+            res.status(500).json({
+                message: 'Error while login user in DB',
                 success: false
             })
         }
