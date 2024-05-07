d9b51f5843b9a6294f3120646a7e157881030143
Kashyap297 <kashyap29700@gmail.com>
2024-05-07 14:47:17 +0530

    problem stuck

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -17,6 +17,7 @@ const authenticate = (req, res, next) => {
         // const payload = jwt.verify(token, Config.JWT_SECRET)
         
         const accessToken = token.split(' ')[1]
+        console.log(accessToken)
 
         const payload = jwt.verify(accessToken, Config.JWT_SECRET, (err, decoded) => {
             if(err){
@@ -27,7 +28,7 @@ const authenticate = (req, res, next) => {
             }
             return decoded
         })
-
+        console.log(payload)
         if(!payload){
             return res.status(401).json({
                 message : "Token not Found or Invalid",
